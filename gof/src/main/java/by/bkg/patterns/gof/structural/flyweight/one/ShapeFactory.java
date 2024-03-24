e44bf3b8c7fcfbfd1db622fb31354c623f6f08e8
alexeyabar <alexeyabar@gmail.com>
2024-03-24 11:39:17 +0300

    Refactoring

--- gof/src/main/java/by/bkg/patterns/gof/structural/flyweight/one/ShapeFactory.java
+++ gof/src/main/java/by/bkg/patterns/gof/structural/flyweight/one/ShapeFactory.java
@@ -8,25 +8,24 @@ import java.util.HashMap;
 
 public class ShapeFactory {
 
-    private static final HashMap<ShapeType, Shape> shapes = new HashMap<ShapeType, Shape>();
+    private static final HashMap<ShapeType, Shape> shapes = new HashMap<>();
 
     public static Shape getShape(ShapeType type) {
         Shape shapeImpl = shapes.get(type);
 
         if (shapeImpl == null) {
-            if (type.equals(ShapeType.OVAL_FILL)) {
-                shapeImpl = new Oval(true);
-            } else if (type.equals(ShapeType.OVAL_NOFILL)) {
-                shapeImpl = new Oval(false);
-            } else if (type.equals(ShapeType.LINE)) {
-                shapeImpl = new Line();
-            }
+            shapeImpl = switch (type) {
+                case OVAL_FILL -> new Oval(true);
+                case OVAL_NOFILL -> new Oval(false);
+                case LINE -> new Line();
+            };
+
             shapes.put(type, shapeImpl);
         }
         return shapeImpl;
     }
 
-    public static enum ShapeType {
-        OVAL_FILL, OVAL_NOFILL, LINE;
+    public enum ShapeType {
+        OVAL_FILL, OVAL_NOFILL, LINE
     }
 }
